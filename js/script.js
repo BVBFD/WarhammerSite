11c100ce2a980ebcccef16a178d64bb32c08242d
BVBFD <lsevina126@gmail.com>
2021-12-09 17:11:51 +0900

    Create the new repository becuz of old repository lfs error

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// const header = document.querySelector(".header");
+// const headerHeight = header.getBoundingClientRect().height;
+const navbarBtn = document.querySelector(".header #menu-btn");
+const navbar = document.querySelector(".header .navShareBox .navbar");
+const videoBoxesBtn = document.querySelectorAll(
+  ".faction .factionBox .btnClickVideo i"
+);
+const factionBoxes = document.querySelectorAll(".faction .factionBox");
+console.log(factionBoxes);
+
+navbarBtn.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+factionBoxes.forEach((factionBox) => {
+  factionBox.onclick = (event) => {
+    if (event.target.parentNode.dataset.src.slice(1, 7) === "videos") {
+      let videoBox = event.target.parentNode.querySelector(".btnClickVideo");
+      videoBox.classList.toggle("active");
+    }
+  };
+});
+
+videoBoxesBtn.forEach((btn) => {
+  btn.onclick = (event) => {
+    event.target.parentNode.parentNode.classList.toggle("active");
+    console.log(
+      event.target.parentNode.parentNode.getElementsByTagName("video")[0]
+    );
+    event.target.parentNode.parentNode.getElementsByTagName("video")[0].pause();
+  };
+});
+
+// document.addEventListener("mouseover", (event) => {
+//   console.log(event.target, headerHeight);
+//   if (event.clientY <= headerHeight) {
+//     header.classList.add("active");
+//   }
+//   if (event.clientY > headerHeight) {
+//     header.classList.remove("active");
+//   }
+// });
